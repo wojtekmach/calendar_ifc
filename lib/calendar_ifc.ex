1debe82d9410ecb9c938376e14860d4cef4fab3b
Wojtek Mach <wojtek@wojtekmach.pl>
2016-04-02 06:43:30 +0200

    Add parse!

--- lib/calendar_ifc.ex
+++ lib/calendar_ifc.ex
@@ -1,2 +1,26 @@
 defmodule Calendar.IFC do
+  @moduledoc ~S"""
+  Calendar.IFC is a library to work with the International Fixed Calendar.
+
+  International Fixed Calendar, or IFC, splits the calendar year into
+  13 months, each having exactly 28 days.
+
+  See: https://en.wikipedia.org/wiki/International_Fixed_Calendar.
+  """
+
+  @doc ~S"""
+  Parses a given date into `Date` struct.
+
+  ## Examples
+
+      iex> Calendar.IFC.parse!({2016, 1, 1})
+      %Date{calendar: Calendar.IFC, year: 2016, month: 1, day: 1}
+  """
+  def parse!({year, month, day} = date) do
+    if day < 0 || day > 13 do
+      raise ArgumentError, "invalid date: #{inspect(date)}"
+    end
+
+    %Date{calendar: __MODULE__, year: year, month: month, day: day}
+  end
 end
