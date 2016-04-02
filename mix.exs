85221657dd1b4c83ba35ad5834110f99f5713ccd
Wojtek Mach <wojtek@wojtekmach.pl>
2016-04-02 06:37:35 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Calendar.IFC.Mixfile do
   def project do
     [app: :calendar_ifc,
      version: "0.0.1",
+     description: "Elixir application to work with International Fixed Calendar",
      elixir: "~> 1.3-dev",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
