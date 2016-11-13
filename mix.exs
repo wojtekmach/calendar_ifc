e5afad25a4369783a22f8c050a809084579b4506
Wojtek Mach <wojtek@wojtekmach.pl>
2016-11-13 01:06:43 +0100

    Depend on Elixir ~> 1.3

--- mix.exs
+++ mix.exs
@@ -5,28 +5,16 @@ defmodule Calendar.IFC.Mixfile do
     [app: :calendar_ifc,
      version: "0.0.1",
      description: "Elixir application to work with International Fixed Calendar",
-     elixir: "~> 1.3-dev",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
